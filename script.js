71ca4a59d38998a9ce946395af9d3125338be71f
Josiah Storrie <jpstorrie@gmail.com>
2022-12-08 11:11:08 -0700

    changed js to a link

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const h2 = document.createElement(`h2`);
-h2.textContent = `This is added by my written JS`;
+h2.textContent = `This is added by my written by `;
 document.querySelector(`body`).appendChild(h2);
-
-
-addEventListener
+const aLine = document.createElement('a')
+aLine.textContent = 'JS'
+aLine.href = 'https://www.javascript.com/'
+aLine.target = `_blank`
+document.querySelector(`h2`).appendChild(aLine)
