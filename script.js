2c88fd63c120b808f23286b04eb3866ffc3e4470
Josiah Storrie <jpstorrie@gmail.com>
2022-12-08 22:52:40 -0700

    added ordered list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement(`h2`);
-h2.textContent = `This is added by my written by `;
+h2.textContent = `A journey of learning to code many languages, including `;
 document.querySelector(`body`).appendChild(h2);
 const aLine = document.createElement('a')
 aLine.textContent = 'JS'
